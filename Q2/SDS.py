b8d0d52da2661db1e7c02c96f2fcece8ca9dc44a
sanchaysingh <sanchay072@gmail.com>
2025-06-27 15:04:43 +0530

    2.1 done

--- Q2/SDS.py
+++ Q2/SDS.py
@@ -151,11 +151,14 @@ class SDS:
         # predict the noise residual with unet, NO grad!
         with torch.no_grad():
             ### YOUR CODE HERE ###
- 
+            noise = torch.randn(latents.shape, dtype=latents.dtype, device=latents.device)
+            noised_sample = self.scheduler.add_noise(latents, noise, t)
+            measured_noise = self.unet(noised_sample, t, encoder_hidden_states=text_embeddings)[0]
 
             if text_embeddings_uncond is not None and guidance_scale != 1:
                 ### YOUR CODE HERE ###
-                pass
+                measured_noise_uncond = self.unet(noised_sample, t, encoder_hidden_states=text_embeddings_uncond)[0]
+                measured_noise = measured_noise + guidance_scale * (measured_noise - measured_noise_uncond)
  
 
 
@@ -164,6 +167,7 @@ class SDS:
         ### YOUR CODE HERE ###
 
 
-        loss = 
+        target = latents - w * grad_scale * (measured_noise - noise)
+        loss = F.mse_loss(latents, target.detach())
 
         return loss
